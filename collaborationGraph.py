b53e6d06287912abca3a1a97da3ee3ee0658f38c
Adel Ahmadyan <ahmadyan@gmail.com>
2016-07-04 16:59:29 -0500

    Added NetworkX as a backend for plotting the graph

--- collaborationGraph.py
+++ collaborationGraph.py
@@ -1,6 +1,14 @@
 import re
 import sys
 import csv
+import matplotlib.pyplot as plt
+import random
+import networkx as nx
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+
 
 def importData(fileName):
     # The CSV file contains the lastname, firstname, [ list of lastnames of co-authors]
@@ -28,7 +36,6 @@ def cleanupData(data):
         row.pop(0)  # remove second row (firstname), which now is the first row
         collaborators.append(row)
 
-
     for collaborator in collaborators:
         edges = []
         for i in range(len(collaborator)):
@@ -39,6 +46,59 @@ def cleanupData(data):
 
     return authors, collaborators, collaboratorsGraph
 
+def generatePlot(graph):
+    scale=0.01
+    circles = []
+    for node in graph:
+        size = len(node)
+        circle = plt.Circle(( random.random()  ,  random.random()  ), size*scale, color='r')
+        circles.append(circle)
+    fig = plt.gcf()
+    for circle in circles:
+        fig.gca().add_artist(circle)
+    plt.axis('off')
+    fig.savefig('plotcircles.png')
+    plt.show()
+
+def draw_graph(G, graph_layout='spring', node_text_size=12, text_font='Merriweather',node_size=1600, node_color='blue', node_alpha=0.4,
+               edge_color='blue', edge_alpha=0.4, edge_tickness=1, edge_text_pos=0.3,):
+    # these are different layouts for the network you may try
+    # shell seems to work best
+    if graph_layout == 'spring':
+        graph_pos = nx.spring_layout(G, iterations=200)
+    elif graph_layout == 'spectral':
+        graph_pos = nx.spectral_layout(G)
+    elif graph_layout == 'random':
+        graph_pos = nx.random_layout(G)
+    else:
+        graph_pos = nx.shell_layout(G)
+
+    # draw graph
+    scale=400
+    nodeSize = []
+    for node in G:
+        d = G.degree(node)
+        if(d<2):
+            d=2
+        #if(d>8):
+        #    d=8
+        nodeSize.append(scale*d)
+
+    nx.draw_networkx_edges(G, graph_pos, width=edge_tickness, alpha=edge_alpha, edge_color=edge_color)
+    nx.draw_networkx_nodes(G, graph_pos, node_size=nodeSize,alpha=node_alpha, node_color=range(len(G.nodes())), cmap=plt.cm.CMRmap)
+    nx.draw_networkx_labels(G, graph_pos, font_size=node_text_size, font_family=text_font)
+
+    # show graph
+    plt.show()
+
+def createNxGraph(authors, collaborators, graph):
+    G=nx.Graph()
+    for i in range(len(authors)):
+        G.add_node(authors[i])
+        for collaborator in collaborators[i]:
+            G.add_edge(authors[i], collaborator)
+    return G
+
 def main():
     fileName = r"collaborationData.csv"
     data = importData(fileName)
@@ -46,4 +106,8 @@ def main():
     print(authors)
     print(collaborators)
     print(graph)
+    nxGraph = createNxGraph(authors, collaborators, graph)
+    #generatePlot(graph)
+    draw_graph(nxGraph)
+
 if  __name__ =='__main__':main()
